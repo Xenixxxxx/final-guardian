664308eed22bc56ddaaec78622db45678b1b2324
Xenix <c348629@gmail.com>
2025-04-05 14:33:53 +1100

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,11 @@ from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 
 load_dotenv(override=True)
 
-token_provider = azure.identity.get_bearer_token_provider(
-    azure.identity.DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-)
-
 llm = AzureChatOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
     azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
     openai_api_version=os.environ["AZURE_OPENAI_VERSION"],
-    azure_ad_token_provider=token_provider,
+    deployment_name=os.environ["AZURE_OPENAI_DEPLOYMENT"],
 )
 
 embedding = AzureOpenAIEmbeddings(
